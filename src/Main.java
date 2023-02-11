5d5282b73f5c6311f5b306d58da3b097754c7f01
Andrew S <samum_94@mail.ru>
2023-02-11 21:13:54 +0300

    Main added

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weightKg = 65.5;
+        double heightM = 1.74;
+        int bmi = (int) service.calculate(weightKg, heightM);
+        System.out.println(bmi);
+    }
 }
